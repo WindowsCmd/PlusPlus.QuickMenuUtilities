364a3e883037ff0b08bebfc12ec6dbb7e179ac63
Windows <killmeuwu@protonmail.com>
2021-11-18 14:08:44 -0800

    Created project

---
+++ QMUtils/QMUtilsMod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MelonLoader;
+
+
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonInfo(typeof(QMUtils.QMUtilsMod), "QuickMenuUtils", "0.0.1", "Astrid")]
+[assembly: MelonColor(ConsoleColor.Red)]
+
+namespace QMUtils
+{
+    public class QMUtilsMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("QuickMenuUtils Starting..");
+
+        }
+    }
+}
---
+++ QMUtils/UIElements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using VRC.UI.Elements;
+
+namespace QMUtils
+{
+    public static class UIElements
+    {
+        public static GameObject quickMenuBase;
+
+        public static IEnumerator GetElements()
+        {
+            while(GameObject.Find("UserInterface").transform.Find("Canvas_QuickMenu/Container/Window/QMParent/Menu_Dashboard/ScrollRect/Viewport/VerticalLayoutGroup/Buttons_QuickActions/") == null)
+                yield return new WaitForEndOfFrame();
+
+            quickMenuBase = GameObject.Find("UserInterface").transform.Find("Canvas_QuickMenu/Container/Window/QMParent/Menu_Dashboard").gameObject;
+        }
+    }
+}
