ed6d408e98992d37f777f9e2275ca01fb4c3fc12
Astrid <killmeuwu@protonmail.com>
2021-12-02 22:42:47 -0800

    created a new project cause other one was bugging and made pages work :3

---
+++ MenuPage.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRC.UI.Core;
+using VRC.UI;
+using VRC.UI.Elements;
+using VRC.UI.Elements.Tooltips;
+using VRC.UI.Elements.Menus;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
+using MelonLoader;
+
+namespace QMUtils
+{
+    public class MenuPage
+    {
+        UIPage page;
+        public GameObject gameObject { get; private set; }
+        public Transform pageContent { get; set; }
+        public RectTransform rectTransform { get; private set; }
+        public VerticalLayoutGroup verticalLayoutGroup { get; private set; }
+        public string pageName { get; private set; }
+        public string title {  get; private set; }
+        public Texture2D icon { get; private set; }
+        public TextMeshProUGUI pageTitle { get; private set; }
+        public RectMask2D mask { get; private set; }
+
+        /// <summary>
+        /// A new quick menu page
+        /// </summary>
+        /// <param name="name">Name of the menu</param>
+        /// <param name="title">The title of the menu, this will be displayed above the page</param>
+        public MenuPage(string name, string title)
+        {
+            gameObject = UnityEngine.Object.Instantiate<GameObject>(UIElements.quickMenuBase, UIElements.quickMenuBase.transform.parent);
+
+            pageName = "Menu_" + name;
+            gameObject.name = pageName;
+            rectTransform = gameObject.GetComponent<RectTransform>();
+
+            GameObject.Destroy(gameObject.GetComponent<UIPage>());
+
+            GameObject.Destroy(rectTransform.Find("Header_H1/RightItemContainer/Button_QM_Expand").gameObject);
+
+            verticalLayoutGroup = rectTransform.Find("ScrollRect/Viewport/VerticalLayoutGroup").GetComponent<VerticalLayoutGroup>();
+
+            for (int i = verticalLayoutGroup.rectTransform.childCount - 1; i >= 0; i--)
+            {
+                MelonLogger.Msg("Destroyed Child " + i);
+                GameObject.Destroy(verticalLayoutGroup.transform.GetChild(i).gameObject);
+            }
+
+            page = gameObject.AddComponent<UIPage>();
+            page.field_Public_String_0 = name;
+            page.field_Public_Boolean_1 = true;
+            page.field_Private_MenuStateController_0 = UIElements.getMenuController();
+            page.field_Private_List_1_UIPage_0 = new Il2CppSystem.Collections.Generic.List<UIPage>();
+            page.field_Private_List_1_UIPage_0.Add(page);
+
+            UIElements.getMenuController().field_Private_Dictionary_2_String_UIPage_0.Add(name, page);
+
+            List<UIPage> updatedUIPages = UIElements.getMenuController().field_Public_ArrayOf_UIPage_0.ToList<UIPage>();
+            updatedUIPages.Add(page);
+            UIElements.getMenuController().field_Public_ArrayOf_UIPage_0 = updatedUIPages.ToArray();
+
+            pageTitle = rectTransform.Find("Header_H1/LeftItemContainer/Text_Title").GetComponent<TextMeshProUGUI>();
+            pageTitle.text = title;
+
+        }
+    }
+}
+ 
---
+++ MenuTab.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using VRC.UI.Core.Styles;
+using VRC.UI.Elements.Controls;
+using VRC.UI.Elements.Controls;
+
+
+namespace QMUtils
+{
+    public class Tab
+    {
+        GameObject root;
+        Image image;
+        MenuTab tab;
+        GameObject menuTabGameObject;
+
+        public Tab(Transform parent, string name, string toolTip, Sprite icon = null)
+        {
+            root = UnityEngine.Object.Instantiate<GameObject>(UIElements.menuTabBase, parent);
+            root.name = name + "_quickMenuUtils";
+            tab = root.gameObject.GetComponent<MenuTab>();
+            tab.field_Private_MenuStateController_0 = UIElements.getMenuController();
+            tab.field_Public_String_0 = name;
+            image = root.transform.Find("Icon").GetComponent<Image>();
+            image.sprite = icon;
+            image.overrideSprite = icon;
+            tab.GetComponent<StyleElement>().field_Private_Selectable_0 = tab.GetComponent<Button>();
+            tab.GetComponent<Button>().onClick.AddListener((Action)(() => tab.GetComponent<StyleElement>().field_Private_Selectable_0 = tab.GetComponent<Button>()));
+        }
+    }
+}
