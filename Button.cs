4965e0d9c0023a3d08a36cd099200c468210e0a7
Windows <killmeuwu@protonmail.com>
2021-12-10 13:54:04 -0800

    asf

--- Button.cs
+++ Button.cs
@@ -15,19 +15,29 @@ namespace QMUtils
         public GameObject gameObject { get; private set; }
         public TextMeshProUGUI buttonText { get; private set; }
         public Button button { get; private set; }
+        public Image buttonIcon { get; private set; }
 
-        public SimpleButton(string text, Transform parent, Action click, string toolTip)
+        public SimpleButton(string text, Transform parent, Action click, string toolTip, Sprite icon = null)
         {
-            MelonLogger.Msg("WTF");
             gameObject = GameObject.Instantiate(UIElements.singleButtonBase, parent);
             buttonText = gameObject.transform.Find("Text_H4").GetComponent<TextMeshProUGUI>();
-            buttonText.gameObject.GetComponent<RectTransform>().localPosition = new Vector3(0f, -25f, 0f);
             buttonText.text = text;
             button = gameObject.GetComponentInChildren<Button>(true);
             button.onClick = new Button.ButtonClickedEvent();
             button.onClick.AddListener(click);
-            GameObject.Destroy(gameObject.transform.Find("Icon").gameObject);
-            GameObject.Destroy(gameObject.transform.Find("Icon_Secondary").gameObject);
+            if(icon != null)
+            {
+                buttonIcon = button.transform.Find("Icon").GetComponentInChildren<Image>(true);
+                buttonIcon.sprite = icon;
+                buttonIcon.overrideSprite = icon;
+                buttonIcon.gameObject.SetActive(true);
+            } 
+            else
+            {
+                buttonText.gameObject.GetComponent<RectTransform>().localPosition = new Vector3(0f, -25f, 0f);
+                GameObject.Destroy(gameObject.transform.Find("Icon").gameObject);
+                GameObject.Destroy(gameObject.transform.Find("Icon_Secondary").gameObject);
+            }
             gameObject.SetActive(true);
         }
     }
